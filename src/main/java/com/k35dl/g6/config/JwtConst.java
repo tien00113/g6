237b1a365f7e9d2e657ab2d87d048f469ceb5d5f
Nguyen Hien Tien <nht010103@gmail.com>
2024-05-15 12:56:33 +0700

    private config

--- src/main/java/com/k35dl/g6/config/JwtConst.java
+++
@@ -1,6 +0,0 @@
-package com.k35dl.g6.config;
-
-public class JwtConst {
-    public static String JWT_HEADER = "Authorization";
-    public static String SECRET_KEY = "nguoitaorakeynaydeptrai10diemthuchonodaihonbinhthuongmotchut";
-}
